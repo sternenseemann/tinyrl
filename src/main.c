f06227c2407d370e08ad7d58d07900f68babb9b3
lukasepple <git@lukasepple.de>
2014-04-12 17:49:17 +0200

    generate monsters and draw them. nothing else

--- src/main.c
+++ src/main.c
@@ -1,16 +1,24 @@
-#include "termbox.h"
+#include "../termbox/termbox.h"
 //#include "debug.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-void draw(int player[2], int level_exit[2])
+#define COUNT_OF( arr) (sizeof(arr)/sizeof(0[arr]))
+
+void draw(int player[2], int level_exit[2], int monsterc, int monsters[][2])
 {
 	tb_clear();
-
+	
+	for(int i = 0; i < monsterc; i++)
+	{
+		tb_change_cell(monsters[i][0],monsters[i][1], 'm', TB_RED, TB_DEFAULT);
+	}
+	
 	tb_change_cell(player[0], player[1], '@', TB_MAGENTA, TB_DEFAULT);
 	tb_change_cell(level_exit[0], level_exit[1], '>',TB_WHITE,TB_DEFAULT);
 
+	
 	tb_present();
 }
 
@@ -23,7 +31,31 @@ int test_position(int x, int y)
 	}
 }
 
-void move(int player[], uint16_t key)
+int test_for_monsters(int x, int y, int monsterc, int monsters[][2])
+{
+	for(int i = 0; i < monsterc; i++)
+	{
+		if(monsters[i][0] == x && monsters[i][1] == y){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void handle_move(int new_x, int new_y, int player[2], int monsterc, int monsters[][2])
+{
+	int monster_there = test_for_monsters(new_x, new_y, monsterc, monsters);
+
+	if(test_position(new_x, new_y) == 1 && monster_there == 0){
+		player[0] = new_x;
+		player[1] = new_y;
+	}
+	else if(monster_there == 1)
+	{
+		// fight 
+	}
+}
+void move(int player[], uint16_t key, int monsterc, int monsters[][2])
 {
 	int new_x, new_y;
 	switch(key)
@@ -31,34 +63,22 @@ void move(int player[], uint16_t key)
 		case TB_KEY_ARROW_UP:
 			new_x = player[0];
 			new_y = player[1] - 1;
-			if(test_position(new_x, new_y) == 1){
-				player[0] = new_x;
-				player[1] = new_y;
-			}
+			handle_move(new_x, new_y, player, monsterc, monsters);
 			break;
 		case TB_KEY_ARROW_DOWN:
 			new_x = player[0];
 			new_y = player[1] + 1;
-			if(test_position(new_x, new_y) == 1){
-				player[0] = new_x;
-				player[1] = new_y;
-			}
+			handle_move(new_x, new_y, player, monsterc, monsters);	
 			break;
 		case TB_KEY_ARROW_LEFT:
 			new_x = player[0] - 1;
 			new_y = player[1];
-			if(test_position(new_x, new_y) == 1){
-				player[0] = new_x;
-				player[1] = new_y;
-			}
+			handle_move(new_x, new_y, player, monsterc, monsters);
 			break;
 		case TB_KEY_ARROW_RIGHT:
 			new_x = player[0] + 1;
 			new_y = player[1];
-			if(test_position(new_x, new_y) == 1){
-				player[0] = new_x;
-				player[1] = new_y;
-			}
+			handle_move(new_x, new_y, player, monsterc, monsters);
 			break;
 		default:
 			break;
@@ -73,7 +93,8 @@ void move(int player[], uint16_t key)
 int main(int argc, char *argv[])
 {
 	int player[2] = { 0, 0 }; // { x-position of the @, y-position of the @ }
-	int level_exit[2]; // { x-pos of exit, y-pos of exit
+	int player_lives = 5;
+	int level_exit[2]; // { x-pos of exit, y-pos of exit }
 	struct tb_event event; // here will our events be stored
 
 	tb_init();
@@ -83,9 +104,18 @@ int main(int argc, char *argv[])
 	level_exit[0] = rand() % tb_width();
 	level_exit[1] = rand() % tb_height();
 
+	int monsterc = rand() % 10;
+	int monsters[monsterc][2];
+	
+	for(int i = 0; i < monsterc; i++)
+	{
+		monsters[i][0] = rand() % tb_width();
+		monsters[i][1] = rand() % tb_height();
+	}
+
 	while(1)
 	{
-		draw(player,level_exit);
+		draw(player, level_exit, monsterc, monsters);
 		tb_poll_event(&event); // wait for an event
 		//debug("got event");
 		switch(event.type)
@@ -98,7 +128,7 @@ int main(int argc, char *argv[])
 						exit(0);
 						break; // yolo
 					default: // let the move-function check if we have to move or not
-						move(player,event.key);
+						move(player,event.key, monsterc, monsters);
 						break;
 				}
 				break;
