f36b8326ba738bccb7c1ffb9db68f50a0cb18958
lukasepple <git@lukasepple.de>
2016-03-13 12:11:06 +0100

    Revert "Use non biased random numbers"
    Can lead to permanent blocking
    
    This reverts commit d7be68a40f889ca4b0756958b27ac28d1ddba2eb.

--- src/util.c
+++ src/util.c
@@ -1,8 +1,4 @@
 #include <stdlib.h>
 int randint(int lower, int upper) {
-	int random;
-	do {
-		random = rand() + lower;
-	} while(random <= upper)
-	return random;
+	return rand() % upper + lower;
 }
